7c251d44b558025d8167550f8eac3c7a7932c389
mpaz <mpazaryna@gmail.com>
2020-09-20 10:33:50 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import Layout from "components/Layout";
+import Bio from "components/Bio";
+import SEO from "components/Seo";
+import { getSortedPosts } from "utils/posts";
+
+export default function Home({ posts }) {
+  return (
+    <Layout>
+      <SEO title="Harry Ohm Life" />
+      <Bio className="my-14" />
+      <a href="/blog">Blog</a>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = getSortedPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
---
+++ site.config.js
@@ -0,0 +1,16 @@
+export default {
+  siteMetadata: {
+    title: "Harry Ohm ",
+    author: {
+      name: "Hare Om",
+      summary: "A Yoga Teaching and Practice.",
+    },
+    description:
+      "A yoga teaching and practice site created with Next.js and Tailwind.css",
+    siteUrl: "https://www.harryohm.life/",
+    language: "en-US",
+    social: {
+      twitter: "yogawithpaz",
+    },
+  },
+};
