1fb173e55f7ea00a89ccd007d461ae28f542a56b
mpaz <mpazaryna@gmail.com>
2020-09-25 10:33:55 -0400

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import "styles/global.css";
-
 import "typeface-open-sans";
 import "typeface-merriweather";
+import { useAnalytics } from "@happykit/analytics";
 
 export default function MyApp({ Component, pageProps }) {
+  // Track page views
+  useAnalytics({ publicKey: "analytics_pub_3ed30e0be4" });
+
   return <Component {...pageProps} />;
 }
